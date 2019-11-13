bb24aff588548d3ed51d3023bd09e644289ecbab
artofnext <novnovalex@gmail.com>
2019-11-13 10:02:39 +0200

    revert button animation, layout update

--- js/convert.js
+++ js/convert.js
@@ -2,7 +2,8 @@ let xmlhttp = new XMLHttpRequest();
 let ratesObj;
 
 xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200 /*&& false*/) {
+  let alert = document.getElementById("base__curr");
+  if (this.readyState == 4 && this.status == 200) {
     ratesObj = JSON.parse(this.responseText);
 
     document.getElementById("base__curr").innerHTML = `Base Currency: ${ratesObj.base} `;
@@ -10,7 +11,7 @@ xmlhttp.onreadystatechange = function() {
 
     ratesObj.rates.EUR = 1;
 
-    console.log(ratesObj);
+    // console.log(ratesObj);
 
     Object.keys(ratesObj.rates).forEach(function(key) {
 
@@ -30,19 +31,25 @@ xmlhttp.onreadystatechange = function() {
     document.getElementById("currencyfrom").disabled = false;
     document.getElementById("currencyto").disabled = false;
     document.getElementById("convert").disabled = false;
+    document.getElementById("rates__button").disabled = false;
+    document.getElementById("button_revert").disabled = false;
+    alert.style.color = 'initial';
 
-    console.log("Enable block");
+    // console.log("Enable block");
 
   } else {
 
-    console.log("Disable block");
+    // console.log("Disable block");
 
-    document.getElementById("base__curr").innerHTML = `Sorry!`;
-    document.getElementById("date").innerHTML = `It seems that there isn't connection!`;
+    alert.innerHTML = `Sorry!`;
+    alert.style.color = 'red';
+    document.getElementById("date").innerHTML = `It seems that there isn't connection!<br>Try refresh the page.`;
     document.getElementById("amount").disabled = true;
     document.getElementById("currencyfrom").disabled = true;
     document.getElementById("currencyto").disabled = true;
     document.getElementById("convert").disabled = true;
+    document.getElementById("rates__button").disabled = true;
+    document.getElementById("button_revert").disabled = true;
   }
 };
 
@@ -60,18 +67,11 @@ function createOptions(elemId, val) {
 
 function getRate(currency) {
 
-  //console.log('Currency to search: ' + currency);
-
   let rate = 0;
 
   Object.keys(ratesObj.rates).forEach(function (key) {
 
-    //console.log('Search: ' + key);
-
     if (currency == key) {
-
-      //console.log('Found: ' + Number(ratesObj.rates[key]));
-
       rate = Number(ratesObj.rates[key]);
     }
 
@@ -89,12 +89,11 @@ function revert() {
   document.getElementById("currencyto").text = document.getElementById("currencyfrom").text;
   document.getElementById("currencyfrom").text = store;
 
-  console.log("Revert block");
+  // console.log("Revert block");
 
-  // document.getElementById('button_revert').style.transition = 'all 0.1s easy';
-  // document.getElementById('button_revert').style.transform = 'rotate(-360deg)';
-  // document.getElementById('button_revert').style.transform = 'rotate(0deg)';
-  // document.getElementById('button_revert').style.display='none';
+  let revertSvg = document.getElementById('revert_icon');
+  revertSvg.style.transition = 'all .3s ease-in';
+  revertSvg.style.transform = (revertSvg.style.transform == 'rotate(-180deg)') ? 'rotate(0deg)' : 'rotate(-180deg)';
 }
 
 function showHide() {
@@ -115,13 +114,13 @@ function convert() {
   let currencyFrom = document.getElementById("currencyfrom").value;
   let currencyTo = document.getElementById("currencyto").value;
 
-  console.log('CurrencyFrom: ' + currencyFrom);
-  console.log('CurrencyTo: ' + currencyTo);
+  // console.log('CurrencyFrom: ' + currencyFrom);
+  // console.log('CurrencyTo: ' + currencyTo);
 
   let rateFrom = getRate(currencyFrom);
   let rateTo = getRate(currencyTo);
 
-  console.log('Rate: ' + rateFrom);
+  // console.log('Rate: ' + rateFrom);
 
   if (rateFrom == 0 || rateTo == 0) {
     alert('Something goes wrong! Try to reload page. If it didn’t help, I am sorry!')
